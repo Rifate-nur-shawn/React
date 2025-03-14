91c30441cee3e8d8d3a25fd2365edad9a73ed6b0
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-03-15 02:13:26 +0600

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,19 @@
+
+import "./App.css";
+import Node from "./Node";
+
+function App() {
+
+
+  return (
+    <>
+      <Node />  
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+        <h1>this is my react project</h1>
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ react/src/Node.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Node = () => {
+  return <div>Node</div>;
+
+};
+
+export default Node;
+const PRODUCT = [
+    { name: "computer", price: 1000, brand: "hp", color: "black" },
+    { name: "laptop", price: 800, brand: "dell", color: "silver" },
+    { name: "phone", price: 800, brand: "dell", color: "silver" },
+    { name: "monitor", price: 800, brand: "dell", color: "silver" },
+    { name: "watch", price: 800, brand: "dell", color: "silver" },
+  ];
+  console.log(PRODUCT);
+    console.log(PRODUCT[0].name);
