8ba8a35114cf5453e33f696fb1e7ec16313ef964
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-03-16 03:24:29 +0600

    prop

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,19 +1,60 @@
-
 import "./App.css";
-import Node from "./Node";
 
 function App() {
-
-
   return (
     <>
-      <Node />  
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-        <h1>this is my react project</h1>
-      </p>
+      {/* <h1> vite + java</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person> */}
+      <Student></Student>
     </>
   );
 }
+//  function Person(){
+
+//    }
+// const money=500;
+// const person={name:'Sakib', age:30}
+// return <h2>name: {person.name} age: {person.age+money}</h2>
+//  }
+
+function Student() {
+  const personStyle = {
+    border: "2px solid red",
+    margin: "5px",
+    padding: "10px",
+    width: "25%"
+  }
+  
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%"
+  }
+  
+  return (
+    <div style={containerStyle}>
+      <div style={personStyle}>
+        <h2>Student</h2>
+        <h3>name: John</h3>
+        <h3>age: 30</h3>
+      </div>
+      <div style={personStyle}>
+        <h2>Student</h2>
+        <h3>name: John</h3>
+        <h3>age: 30</h3>
+      </div>
+      <div style={personStyle}>
+        <h2>Student</h2>
+        <h3>name: John</h3>
+        <h3>age: 30</h3>
+      </div>
+    </div>
+  )
+}
+
+
 
 export default App;
